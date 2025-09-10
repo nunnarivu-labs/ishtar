7c38725dc3dac9034cd886453ffc7c796326f853
Johnson Abraham <johnson.abraham@anthology.com>
2025-09-10 06:05:35 +0530

    updates to file.ts

--- src/utilities/file.ts
+++ src/utilities/file.ts
@@ -1,21 +1,9 @@
-const ALLOWED_IMG_MIME_TYPES = [
-  'image/jpeg',
-  'image/png',
-  'image/gif',
-  'image/webp',
-];
+const PDF_TYPE = 'application/pdf';
+const IMG_REGEX = /^image\/.+$/;
 
-const ALLOWED_DOC_MIME_TYPES = ['application/pdf'];
+export const isImage = (type: string) => IMG_REGEX.test(type);
 
-export const ALLOWED_MIME_TYPES = [
-  ...ALLOWED_IMG_MIME_TYPES,
-  ...ALLOWED_DOC_MIME_TYPES,
-];
+export const isDocument = (type: string) => type === PDF_TYPE;
 
 export const isAllowedType = (type: string) =>
-  ALLOWED_MIME_TYPES.includes(type);
-
-export const isImage = (type: string) => ALLOWED_IMG_MIME_TYPES.includes(type);
-
-export const isDocument = (type: string) =>
-  ALLOWED_DOC_MIME_TYPES.includes(type);
+  isImage(type) || isDocument(type);
