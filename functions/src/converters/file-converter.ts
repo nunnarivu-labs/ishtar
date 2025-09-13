8160be5aa48cdf7c005a9868b03aa7727faa66bb
Johnson Abraham <johnson.abraham@anthology.com>
2025-09-13 22:05:19 +0530

    file converter for functions

---
+++ functions/src/converters/file-converter.ts
@@ -0,0 +1,32 @@
+import { QueryDocumentSnapshot } from 'firebase-admin/firestore';
+import { DocumentData } from 'firebase/firestore';
+import admin from 'firebase-admin';
+import { File } from '@ishtar/commons/types';
+
+export const fileConverter = {
+  toFirestore: (file: File): DocumentData => {
+    return {
+      originalFileName: file.originalFileName,
+      storagePath: file.storagePath,
+      type: file.type,
+      url: file.url,
+      createdAt:
+        file.createdAt instanceof Date
+          ? admin.firestore.Timestamp.fromDate(file.createdAt)
+          : file.createdAt,
+    };
+  },
+  fromFirestore: (snapshot: QueryDocumentSnapshot<File>): File => {
+    const data = snapshot.data();
+    return {
+      id: snapshot.id,
+      originalFileName: data.originalFileName,
+      storagePath: data.storagePath,
+      type: data.type,
+      url: data.url,
+      createdAt: (
+        data.createdAt as unknown as admin.firestore.Timestamp
+      ).toDate(),
+    };
+  },
+};
