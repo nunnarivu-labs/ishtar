65deb42ef98181a62ff9a1ed44f286136c9b1f58
Johnson Abraham <johnson.abraham@anthology.com>
2025-09-13 15:12:19 +0530

    file fetch and converter

---
+++ src/converters/file-converter.ts
@@ -0,0 +1,23 @@
+import type { File } from '@ishtar/commons/types';
+import { converter } from './converters.ts';
+import { Timestamp } from 'firebase/firestore';
+
+export const fileConverter = converter<File>({
+  toFirestore: (file) => ({
+    originalFileName: file.originalFileName,
+    storagePath: file.storagePath,
+    type: file.type,
+    createdAt:
+      file.createdAt instanceof Date
+        ? Timestamp.fromDate(file.createdAt)
+        : file.createdAt,
+  }),
+
+  fromFirestore: (id, file) => ({
+    id,
+    originalFileName: file.originalFileName,
+    storagePath: file.storagePath,
+    type: file.type,
+    createdAt: (file.createdAt as unknown as Timestamp).toDate(),
+  }),
+});
