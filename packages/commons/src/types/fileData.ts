b56e16cb8162b63a94f0c93e04c16be793d2f9bd
Johnson Abraham <johnson.abraham@anthology.com>
2025-09-13 22:29:00 +0530

    changed file to fileData.ts

--- packages/commons/src/types/fileData.ts
+++ packages/commons/src/types/fileData.ts
@@ -1,4 +1,4 @@
-export type File = {
+export type FileData = {
   id: string;
   originalFileName: string;
   storagePath: string;
--- packages/commons/src/types/index.ts
+++ packages/commons/src/types/index.ts
@@ -4,4 +4,4 @@ export * from './ai-models';
 export * from './ai-request';
 export * from './global-settings';
 export * from './user';
-export * from './file';
+export * from './fileData.ts';
