68b106d5f0b29ee061d819137c5d54f5eff752f8
Johnson Abraham <johnsonabraham0812@gmail.com>
2025-09-15 15:11:30 +0530

    Feature/nano banana (#38)

--- src/components/messages/model-message.tsx
+++ src/components/messages/model-message.tsx
@@ -1,12 +1,19 @@
 import type { Message } from '@ishtar/commons/types';
 import { Markdown } from '../markdown.tsx';
+import { Fragment } from 'react';
+import { FileContent } from './content/file-content.tsx';
 
 type ModelMessageProps = {
   message: Message;
 };
 
 export const ModelMessage = ({ message }: ModelMessageProps) => {
-  const content = message.contents.find((content) => content.type === 'text');
+  const { contents } = message;
 
-  return content ? <Markdown text={content.text} /> : null;
+  return contents.map((content, index) => (
+    <Fragment key={index}>
+      {content.type === 'text' ? <Markdown text={content.text} /> : null}
+      {content.type === 'file' ? <FileContent content={content} /> : null}
+    </Fragment>
+  ));
 };
--- src/data/global-settings.ts
+++ src/data/global-settings.ts
@@ -6,6 +6,7 @@ export const getGlobalSettings = (role: UserRole): GlobalSettings => ({
     role === 'admin'
       ? [
           'gemini-2.5-pro',
+          'gemini-2.5-flash-image-preview',
           'gemini-2.5-flash',
           'gemini-2.5-flash-lite',
           'gemini-2.0-flash',
